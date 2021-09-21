2a500cee54b6fde8a25e17200cb10b90d1375b5b
jonaproitz <jonaproitz@gmail.com>
2021-09-22 00:09:33 +0200

    Move max_offdiag_symmetric function to header file

---
+++ Problems/max_offdiag_symmetric.hpp
@@ -0,0 +1,21 @@
+
+
+#include <armadillo>
+#include <iostream>
+
+double max_offdiag_symmetric(const arma::mat& A, int& k, int &l){
+    int n = A.col(0).n_elem - 1;
+    double a_max;
+
+    for (int i = 1; i < n; i++){
+        for (int j = i+1; j < n; j++){
+            double val = fabs(A(i, j));
+            if (val > a_max){
+                a_max = val; k = i; l = j;
+            }
+        }
+    }
+    
+    // Return matrix element
+    return A(k, l);
+}
