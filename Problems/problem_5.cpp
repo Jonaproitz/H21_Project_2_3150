d78c6a1c6439c1ea51057c5959d5646346eff057
jonaproitz <jonaproitz@gmail.com>
2021-09-22 01:44:50 +0200

    Add necessary computations before rotation

--- Problems/problem_5.cpp
+++ Problems/problem_5.cpp
@@ -4,9 +4,28 @@
 
 
 void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
-    // Necessary computations
+    // Necessary computations before rotation
+    double t, c, s;
 
-    
+    if (A(k, l) != 0){
+        double tau = (A(l ,l) - A(k, k)) / (2*A(k, l));
+        if (tau > 0){
+            t = 1 / (tau + sqrt(1 + tau*tau));
+        }
+        else{
+            t = -1 / (-tau + sqrt(1 + tau*tau));
+        }
+
+        double c = 1 / sqrt(1 + t*t);
+        double s = c*t;
+    }
+    else{
+        t = 0;
+        c = 1;
+        s = 0;
+    }
+
+
     // Perform a single jacobi rotation
 
 
@@ -34,7 +53,7 @@ void jacobi_eigensolver(const arma::mat& A, double eps, arma::vec& eigenvalues,
 
 
     // Set bool reference "converged" to true
-    
+
 
     // Store eigenvalues and eigenvectors
 
