ae28647c44d3f4b70cc733f71c67308ff968dd75
jonaproitz <jonaproitz@gmail.com>
2021-09-21 16:04:09 +0200

    Setup constants in diagonals

--- Problems/problem_3.cpp
+++ Problems/problem_3.cpp
@@ -2,13 +2,23 @@
 #include<armadillo>
 
 int main(){
+    // Setup constants 
     int N = 6;
+    double L = 1.;
+    double h = L/(N-1);
+
+    double a = -1/h*h;
+    double d = 2/h;
+
+    // Setup tridiagonal matrix A
     arma::mat A = arma::mat(N, N).fill(0.);
-    arma::vec d = arma::vec(6).fill(1.);
-    arma::vec a = arma::vec(5). fill(2.);
-    A.diag() = d;
-    A.diag(1) = a;
-    A.diag(-1) = a;
+    arma::vec b = arma::vec(N).fill(d);
+    arma::vec ac = arma::vec(N-1). fill(a);
+    A.diag() = b;
+    A.diag(1) = ac;
+    A.diag(-1) = ac;
     A.print();
+
+    // End program
     return 0;
 }
