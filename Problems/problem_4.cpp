08e152ef71565b6cca6a38d6314827fe885b0fec
jonaproitz <jonaproitz@gmail.com>
2021-09-21 23:34:48 +0200

    Test that max_offdiag_symmetric works for given matrix

--- Problems/problem_4.cpp
+++ Problems/problem_4.cpp
@@ -20,7 +20,17 @@ double max_offdiag_symmetric(const arma::mat& A, int& k, int &l){
 }
 
 int main(){
-
+    // Call max_offdiagonal_symmetric function
+    arma::mat A = {
+        {1., 0., 0., 0.5},
+        {0., 1., -0.7, 0.},
+        {0., -0.7, 1., 0.},
+        {0.5, 0., 0., 1.}
+        };
+    int k = 1;
+    int l = 1;
+    double a = max_offdiag_symmetric(A, k ,l);
+    std::cout << a << "\n";
     
     // End program
     return 0;
