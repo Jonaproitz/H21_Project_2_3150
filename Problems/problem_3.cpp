bac4a44ee9a38466d4208c085315839b889b264a
jonaproitz <jonaproitz@gmail.com>
2021-09-21 15:51:25 +0200

    Fill super- and sub diagonal with twos

--- Problems/problem_3.cpp
+++ Problems/problem_3.cpp
@@ -5,7 +5,10 @@ int main(){
     int N = 6;
     arma::mat A = arma::mat(N, N).fill(0.);
     arma::vec d = arma::vec(6).fill(1.);
+    arma::vec a = arma::vec(5). fill(2.);
     A.diag() = d;
+    A.diag(1) = a;
+    A.diag(-1) = a;
     A.print();
     return 0;
 }
