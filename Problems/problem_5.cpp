04213fa6bec0436cba28fefc7fd8277faeb2bb47
jonaproitz <jonaproitz@gmail.com>
2021-09-22 01:30:36 +0200

    Setup program structure

--- Problems/problem_5.cpp
+++ Problems/problem_5.cpp
@@ -3,6 +3,47 @@
 #include "max_offdiag_symmetric.hpp"
 
 
+void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
+    // Necessary computations
+
+    
+    // Perform a single jacobi rotation
+
+
+    // End funtion with no return
+    return;
+}
+
+
+void jacobi_eigensolver(const arma::mat& A, double eps, arma::vec& eigenvalues, 
+                        arma::mat& eigenvectors, const int maxiter, int& iterations, 
+                        bool& converged){
+    // Create necessary inputs for jacobi_rotate
+
+    
+    // Find original indices of largest off-diagonal matrix element and abs(a_max)
+    
+
+    // Loop Jacobi_rotate until abs(a_max)<eps
+
+
+        // Stops if the number of iterations reaches "maxiter"
+
+
+    // Store number of iterations in integer "iterations"
+
+
+    // Set bool reference "converged" to true
+    
+
+    // Store eigenvalues and eigenvectors
+
+
+    // End function with no return
+    return;
+}
+
+
 int main(){
 
     // End program
