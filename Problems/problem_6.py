72f2ab04c25b0669a277584815fa5a038d7518a7
jonaproitz <jonaproitz@gmail.com>
2021-09-22 21:42:11 +0200

    Find and store number of iterations for each N

--- Problems/problem_6.py
+++ Problems/problem_6.py
@@ -4,3 +4,10 @@ import os
 
 
 N = [N for N in range(2, 10)]
+iterations = []
+
+for N_i in N:
+    os.system(f"~/Documents/Sem_7/FYS3150/Project_2/Problems/Problem_6.exe {N_i}")
+    with open("iterations.txt") as file:
+        iterations.append(int(file.readline()))
+
