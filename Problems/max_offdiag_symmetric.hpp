0060d082decedf341fb8aabf4aa56bdda5eb9125
jonaproitz <jonaproitz@gmail.com>
2021-09-22 15:50:44 +0200

    Fix boundries in loop

--- Problems/max_offdiag_symmetric.hpp
+++ Problems/max_offdiag_symmetric.hpp
@@ -2,10 +2,10 @@
 #include "tridiag_matrix.hpp"
 
 double max_offdiag_symmetric(const arma::mat& A, int& k, int &l){
-    int n = A.n_rows - 1;
+    int n = A.n_rows;
     double a_max;
 
-    for (int i = 1; i < n; i++){
+    for (int i = 0; i < n; i++){
         for (int j = i+1; j < n; j++){
             double val = fabs(A(i, j));
             if (val > a_max){
