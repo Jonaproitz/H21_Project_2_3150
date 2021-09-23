736451a1f762198d86041a416858ce237fc6f64e
jonaproitz <jonaproitz@gmail.com>
2021-09-23 10:21:34 +0200

    Solve eigenfunction

--- Problems/problem_7.cpp
+++ Problems/problem_7.cpp
@@ -3,16 +3,25 @@
 
 int main(){
     int n = 10;
-    double L = 1.;
 
-    float h = L / (n - 1);
+    float h = 1. / (n - 1);
 
     int N = n - 2;
 
-    arma::vec x = arma::linspace(h, L - h, N);
+    arma::vec x = arma::linspace(h, 1. - h, N);
 
 
     double a = -1 / (h*h);
     double d = 2 / h;
     arma::mat A = create_tridiag_matrix(N, a, d);
+
+
+    double eps = 1e-8;
+    arma::vec eigenvalues;
+    arma::mat eigenvectors;
+    int maxiter = 1e6, iterations;
+    bool converged;
+    jacobi_eigensolver(A, eps, eigenvalues, eigenvectors, maxiter, iterations, converged);
+
+    return 0;
 }
