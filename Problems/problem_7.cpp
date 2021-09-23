f4485a68013215272f16f9e27ecf97a45b69add8
jonaproitz <jonaproitz@gmail.com>
2021-09-23 09:52:07 +0200

    create x vector

--- Problems/problem_7.cpp
+++ Problems/problem_7.cpp
@@ -1,3 +1,13 @@
 
 #include "jacobi_eigensolver.hpp"
 
+int main(){
+    int n = 10;
+    double L = 1.;
+
+    float dn = L / (n - 1);
+
+    int N = n - 2;
+
+    arma::vec x = arma::linspace(dn, L - dn, N);
+}
