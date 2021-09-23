84c0fe99dc9fe4990b7176ab8e0eee44854598bd
jonaproitz <jonaproitz@gmail.com>
2021-09-23 13:04:19 +0200

    Load analytic solution

--- Problems/problem_7.py
+++ Problems/problem_7.py
@@ -3,13 +3,19 @@ import matplotlib.pyplot as plt
 import pyarma
 
 
-filename = "solution.bin"
+filename_1 = "solution.bin"
 solution = pyarma.mat()
-solution.load(filename)
+solution.load(filename_1)
 
 sol = np.transpose(np.array(solution))
 x = sol[0]
 V = sol[1:]
+
+filename_2 = "analytic_solution.bin"
+analytic_solution = pyarma.mat()
+analytic_solution.load(filename_2)
+
+U = np.transpose(np.array(analytic_solution))
 
 plt.figure()
 
