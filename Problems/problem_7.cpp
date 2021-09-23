81c5a6deb2d48158105800280461eb97b026dc81
jonaproitz <jonaproitz@gmail.com>
2021-09-23 09:58:35 +0200

    Create matrix A

--- Problems/problem_7.cpp
+++ Problems/problem_7.cpp
@@ -5,9 +5,14 @@ int main(){
     int n = 10;
     double L = 1.;
 
-    float dn = L / (n - 1);
+    float h = L / (n - 1);
 
     int N = n - 2;
 
-    arma::vec x = arma::linspace(dn, L - dn, N);
+    arma::vec x = arma::linspace(h, L - h, N);
+
+
+    double a = -1 / (h*h);
+    double d = 2 / h;
+    arma::mat A = create_tridiag_matrix(N, a, d);
 }
